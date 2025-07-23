07232f477ad86081a0727548c9f449eaab503c2a
lucg-gith <garrelucia@gmail.com>
2025-07-23 21:00:29 +0100

    add: Home content & logo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
       <header style={{ backgroundColor: "Black" }}>
         <img
-          src="https://www.google.com/imgres?q=logo%20tester&imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F023%2F070%2F399%2Fnon_2x%2Ftester-rubber-stamp-seal-vector.jpg&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Fvector-art%2F23070399-tester-rubber-stamp-seal-vector&docid=KWcNZYJcVvPxrM&tbnid=hQWdq_oiPNTfWM&vet=12ahUKEwiWqt62s9OOAxV0Z0EAHavrNoQQM3oECBQQAA..i&w=1565&h=980&hcb=2&ved=2ahUKEwiWqt62s9OOAxV0Z0EAHavrNoQQM3oECBQQAA"
+          src="https://fish.audio/_next/image/?url=https%3A%2F%2Fpublic-platform.r2.fish.audio%2Fcoverimage%2F8d63623ecbf54f359eea60b5b58a361e&w=256&q=75"
           alt="Logo image"
         />
         <nav>
